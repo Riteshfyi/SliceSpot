6502ca12034e5d70cd3c7f5b53cc624622b8053a
Ritesh <riteshfyi@gmail.com>
2024-06-25 01:25:25 +0530

    logout-fixes

--- slicespot-api/Controller/Auth.js
+++ slicespot-api/Controller/Auth.js
@@ -191,8 +191,8 @@ exports.logout = async (req, res) => {
         
       //res.cookie('token', '', { expires: new Date(0) });
 
-      res.clearCookie('token');
-      
+      res.clearCookie('token' , {domain:'https://slicespot.vercel.app' , path:'/'});
+
     res.status(200).json({
       success: true,
       message: "logged out successfully",
